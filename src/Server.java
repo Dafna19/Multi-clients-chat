8402d27711e78d5ec8a14d5ebbeedf2ebed2789c
Natalia <stepanishina96@gmail.com>
2016-10-27 08:24:09 +0300

    server part

--- src/Server.java
+++ src/Server.java
@@ -5,15 +5,15 @@ import java.util.LinkedList;
 /**
  * Адреса и порты задаются через командную строку:
  * клиенту --- куда соединяться, серверу --- на каком порту слушать.
- *
- * * * файл с логинами и паролями пользователей
- *
  * Написать текстовый многопользовательский чат.
  * Пользователь управляет клиентом. На сервере пользователя нет.
  * Сервер занимается пересылкой сообщений между клиентами
  * По умолчанию сообщение посылается всем участникам чата
  * Есть команда послать сообщение конкретное пользователи (@senduser Vasya)
  * Программа работает по протоколу TCP.
+ *
+ *
+ * * * файл с логинами и паролями пользователей
  *
  * Сервер подключает клиентов и только
  *
@@ -23,19 +23,20 @@ public class Server {
     private LinkedList<ClientInfo> clients = new LinkedList<>();
 
     public void main(String[] args) throws IOException {
-        System.out.println("Welcome!");
+        System.out.println("I'm working...");
         int port = Integer.parseInt(args[0]);
         ServerSocket ss = new ServerSocket(port);
 
         while(true){
             try {
-                Socket socket = ss.accept();
-                ServerClient client = new ServerClient(socket, clients);
-                client.start();
+                Socket socket = ss.accept();    //подсоединение
+                System.out.println("New client.");
                 ClientInfo info = new ClientInfo(socket);
                 synchronized (clients){
-                    clients.add(info);
+                    clients.add(info);  //добавляем в список
                 }
+                ServerClient client = new ServerClient(socket, clients);
+                client.start(); //запускаем
             } catch (IOException e) {
                 e.printStackTrace();
             }
--- src/ServerClient.java
+++ src/ServerClient.java
@@ -9,15 +9,13 @@ import java.util.ListIterator;
 public class ServerClient extends Thread {
     private LinkedList<ClientInfo> allClients;
     private Socket socket;
-    private String name = "";
-    public DataOutputStream out;
+    private String myName = "";
     public DataInputStream in;
 
     public ServerClient(Socket s, LinkedList list) {
         socket = s;
         allClients = list;
         try {
-            out = new DataOutputStream(socket.getOutputStream());
             in = new DataInputStream(socket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
@@ -29,25 +27,45 @@ public class ServerClient extends Thread {
             while (true) {
                 String line;
                 line = in.readUTF();
-                synchronized (allClients) {
-                    ListIterator<ClientInfo> it = allClients.listIterator();
-                    while (it.hasNext()) {
-                        Socket toSocket = it.next().getSocket();
-                        if (!toSocket.equals(socket))//себе не отправлять
-                            try {
-                                new DataOutputStream(toSocket.getOutputStream()).writeUTF(line);//отправляем всем
-                            } catch (IOException e) {
-                                e.printStackTrace();
+                //клмент посылает без имени!
+                if (line.contains("@quit")) {
+                    int space = line.indexOf(" ");//находим конец имени
+                    String name = line.substring(0, space-1);//без ":"
+                    System.out.println(name + " is quited");
+
+                    //как-то удалить его из списка!
+                }
+                else if(line.contains("@")){//отправляем кому-то
+                    int at = line.indexOf("@");
+                    int space = line.indexOf(" ", at);//находим конец имени
+                    String name = line.substring(at+1, space);//имя получателя
+                    line = line.substring(space+1);
+                    //поиск клиента в списке
+                    synchronized (allClients){
+                        ListIterator<ClientInfo> it = allClients.listIterator();
+                        while (it.hasNext()) {
+                            ClientInfo receiver = it.next();
+                            if(name.equals(receiver.getName())){
+                                new DataOutputStream(receiver.getSocket().getOutputStream()).writeUTF(myName + ": " + line);//отправляем
                             }
+                        }
                     }
                 }
-
-           /* if (line.equals("@quit")) {
-                System.out.println("client is quited");
-                break;
-            }*/
+                else
+                    synchronized (allClients) { //отправляет всем
+                        ListIterator<ClientInfo> it = allClients.listIterator();
+                        while (it.hasNext()) {
+                            Socket toSocket =  it.next().getSocket();
+                            if (!toSocket.equals(socket))//себе не отправлять
+                                try {
+                                    new DataOutputStream(toSocket.getOutputStream()).writeUTF(myName + ": " + line);//отправляем всем
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                        }
+                    }
             }
-        }catch(IOException e) {//socket closed
+        } catch (IOException e) {//socket closed
         }
     }
 }
